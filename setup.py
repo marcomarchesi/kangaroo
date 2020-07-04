8b30c13721ec26a7e981be088085a460a9174b0a
marcomarchesi <marchesimarco@gmail.com>
2020-07-05 00:40:34 +0100

    setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name="kangaroo-build",
-   version="1.0.9",
+   version="1.0.10",
    description="keep track of your build number, that's it.",
    long_description="a build tracker",
    author="Marco Marchesi",
