a524a529cf63e9b3bd43a574769ecfbf22870c1d
marcomarchesi <marchesimarco@gmail.com>
2020-07-04 17:20:58 +0100

    wip ci

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name="kangaroo",
-   version="1.0.8",
+   version="1.0.9",
    description="keep track of your build number, that's it.",
    author="Marco Marchesi",
    author_email="marchesimarco@gmail.com",
